5031e5d9c420f2d8efac8ddc7793182bdc9b7519
Sammy G <18464241+SAMMY0909@users.noreply.github.com>
2022-04-28 20:16:10 +0200

    Add files via upload

---
+++ plot_c_dist.py
@@ -0,0 +1,160 @@
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+import tables as tb
+from h5py import File
+import os
+
+input_file_std = "/home/sammy/eos/user/s/sgoswami/public/NN_SRC_DATA/ttbar/std/refined_std_tightuser.sgoswami.28416367._000237.output.h5"
+input_file_lrt = "/home/sammy/eos/user/s/sgoswami/public/NN_SRC_DATA/ttbar/lrt/refined_lrt_tightuser.sgoswami.28415580._000203.output.h5"
+
+
+stndrd = 0
+lrt = 0
+
+Bins = {
+  "numberOfInnermostPixelLayerHits" : np.arange(5),
+  "numberOfNextToInnermostPixelLayerHits": np.arange(5),
+  "numberOfInnermostPixelLayerSharedHits": np.arange(5),
+  "numberOfInnermostPixelLayerSplitHits" : np.arange(5),
+  "numberOfPixelHits" : np.arange(10),
+  "numberOfPixelHoles": np.arange(10),
+  "numberOfPixelSharedHits" : np.arange(10),
+  "numberOfPixelSplitHits"  : np.arange(10),
+  "numberOfSCTHits" : np.arange(30),
+  "numberOfSCTHoles": np.arange(30),
+  "numberOfSCTSharedHits" : np.arange(30),
+  "expectNextToInnermostPixelLayerHit" : np.arange(5),
+  "expectInnermostPixelLayerHit" : np.arange(5),
+  "radiusOfFirstHit" : np.arange(0, 150, 10),
+  "chiSquared" : np.arange(0, 50, 5),
+  "numberDoF" : np.arange(0, 50, 5),
+  "ptfrac": np.arange(0, 1, 10),
+  "pt"  : np.arange(0, 10000, 500),
+  "eta" : np.arange(-3, 3, 0.1),
+  "deta" : np.arange(0, 6, 0.1),
+  "dphi" : np.arange(0, 5, 0.1),
+  "theta" : np.arange(0, 5, 0.1),
+  "dr" : np.arange(0, 5, 0.1),
+  "qOverP" : np.arange(0, 0.001, 0.0001),
+  "qOverPUncertainty" : np.arange(0, 0.001, 0.0001),
+  "phiUncertainty" : np.arange(0, 5, 0.1),
+  "thetaUncertainty" : np.arange(0, 5, 0.1),
+  "IP3D_signed_d0" : np.arange(-10, 10, 1),
+  "IP2D_signed_d0" : np.arange(-10, 10, 1),
+  "IP3D_signed_z0" : np.arange(-30, 30, 1),
+  "d0" : np.arange(-10, 10, 1),
+  "z0SinTheta" : np.arange(-30, 30, 1),
+  "d0Uncertainty" : np.arange(-1, 1, 0.1),
+  "z0SinThetaUncertainty" :  np.arange(-3, 3, 0.1),
+  "IP3D_signed_d0_significance" : np.arange(-30, 30, 1),
+  "IP3D_signed_z0_significance" : np.arange(-50, 50, 1),
+  "z0RelativeToBeamspot": np.arange(-50, 50, 1),
+  "z0RelativeToBeamspotUncertainty" : np.arange(-5, 5, 0.1),
+}
+
+variables = {
+    "hits": [
+        "numberOfInnermostPixelLayerHits",
+        "numberOfNextToInnermostPixelLayerHits",
+        "numberOfInnermostPixelLayerSharedHits",
+        "numberOfInnermostPixelLayerSplitHits",
+        "numberOfPixelHits",
+        "numberOfPixelHoles",
+        "numberOfPixelSharedHits",
+        "numberOfPixelSplitHits",
+        "numberOfSCTHits",
+        "numberOfSCTHoles",
+        "numberOfSCTSharedHits",
+        "expectNextToInnermostPixelLayerHit",
+        "expectInnermostPixelLayerHit",
+        "radiusOfFirstHit",
+    ],
+    "quality": [
+        "chiSquared",
+        "numberDoF",
+        "ptfrac",
+    ],
+    "kine": [
+        "pt",
+        "eta",
+        "deta",
+        "dphi",
+        "theta",
+        "dr",
+        "qOverP",
+        "qOverPUncertainty",
+        "phiUncertainty",
+        "thetaUncertainty",
+    ], 
+    "ips": [
+        "IP3D_signed_d0",
+        "IP2D_signed_d0",
+        "IP3D_signed_z0",
+        "d0",
+        "z0SinTheta",
+        "d0Uncertainty",
+        "z0SinThetaUncertainty",
+        "IP3D_signed_d0_significance",
+        "IP3D_signed_z0_significance",
+        "z0RelativeToBeamspot",
+        "z0RelativeToBeamspotUncertainty"
+    ], 
+}
+
+b_tracks_std = 0
+b_tracks_lrt = 0
+
+with File(input_file_std, 'r') as h5files:
+    tracks = h5files['tracks_from_jet']
+    #tracks = h5files['tracks']
+    jets = h5files['jets']
+    bjets = (jets["HadronConeExclTruthLabelID"] == 4).nonzero()[0]
+    b_tracks_std = tracks[bjets,:]
+    rownum,colnum=b_tracks_std.shape
+    print("The dimensions of the array for std container is, after cuts: " +str(rownum)+" by "+str(colnum))
+
+with File(input_file_lrt, 'r') as h5filel:
+    tracks = h5filel['tracks_from_jet']
+    #tracks = h5filel['tracks']
+    jets = h5filel['jets']
+    bjets = (jets["HadronConeExclTruthLabelID"] == 4).nonzero()[0]
+    b_tracks_lrt = tracks[bjets,:]
+    rownum,colnum=b_tracks_lrt.shape
+    print("The dimensions of the array for std+LRT container is, after cuts: " +str(rownum)+" by "+str(colnum))
+
+os.makedirs('cplots_new',exist_ok = True)
+os.chdir('cplots_new')
+
+for vartype in variables:
+    print("The vartype is: "+ vartype+ "\n")
+    for var in variables[vartype]:
+        print("The var is: "+ var +"\n")
+        stndrd = b_tracks_std[b_tracks_std["valid"] == 1][var]
+        lrt = b_tracks_lrt[b_tracks_lrt["valid"] == 1][var]
+            
+        plt.clf()
+            
+        fig,ax=plt.subplots(2,1)
+            
+        val_of_bins_x1, edges_of_bins_x1, patches_x1=ax[0].hist(stndrd, color = ['r'], bins = Bins[var], histtype='stepfilled', alpha=0.5, density=True, label="ttbar STD corr to c-jets")
+        val_of_bins_x2, edges_of_bins_x2, patches_x2=ax[0].hist(lrt, color = ['g'], bins = Bins[var], histtype='stepfilled', alpha=0.5, density=True, label="ttbar STD+LRT corr to c-jets")
+            
+        pltratio = np.divide(val_of_bins_x1,val_of_bins_x2,where=(val_of_bins_x2 != 0))
+        plterror = np.divide(val_of_bins_x1 * np.sqrt(val_of_bins_x2) + val_of_bins_x2 * np.sqrt(val_of_bins_x1),np.power(val_of_bins_x2, 2),where=(val_of_bins_x2 != 0))
+            
+        bincenter = 0.5 * (edges_of_bins_x1[1:] + edges_of_bins_x1[:-1])
+            
+        ax[0].legend(loc='upper right',fontsize=10,framealpha=0.2)
+        #ax[0].set_xlabel(var, size=10)
+        ax[0].set_ylabel("Normalized distribution \n (Method=density)", size=10)
+            
+        #ax[1].errorbar(bincenter, pltratio, yerr=plterror, fmt='.', color='b')
+        ax[1].hist(pltratio, color = ['k'], bins = Bins[var], histtype='step',density=True, label="ratio")
+        ax[1].set_xlabel(var, size=10)
+        ax[1].set_ylabel("Ratio", size=10)
+        
+        plname=str(var)+'_charm.png'
+        fig.savefig(plname,bbox_inches="tight")
+        print("Savefig block done for "+var+".\n")
---
+++ plot_comb_lrt.py
@@ -0,0 +1,152 @@
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+import tables as tb
+from h5py import File
+import os
+
+input_file_lrt = "/home/sammy/eos/user/s/sgoswami/public/NN_SRC_DATA/ttbar/lrt/refined_lrt_tightuser.sgoswami.28415580._000200.output.h5"
+
+lrt_l = 0
+lrt_c = 0
+lrt_b = 0
+
+Bins = {
+  "numberOfInnermostPixelLayerHits" : np.arange(5),
+  "numberOfNextToInnermostPixelLayerHits": np.arange(5),
+  "numberOfInnermostPixelLayerSharedHits": np.arange(5),
+  "numberOfInnermostPixelLayerSplitHits" : np.arange(5),
+  "numberOfPixelHits" : np.arange(10),
+  "numberOfPixelHoles": np.arange(10),
+  "numberOfPixelSharedHits" : np.arange(10),
+  "numberOfPixelSplitHits"  : np.arange(10),
+  "numberOfSCTHits" : np.arange(30),
+  "numberOfSCTHoles": np.arange(30),
+  "numberOfSCTSharedHits" : np.arange(30),
+  "expectNextToInnermostPixelLayerHit" : np.arange(5),
+  "expectInnermostPixelLayerHit" : np.arange(5),
+  "radiusOfFirstHit" : np.arange(0, 150, 10),
+  "chiSquared" : np.arange(0, 50, 5),
+  "numberDoF" : np.arange(0, 50, 5),
+  "ptfrac": np.arange(0, 1, 10),
+  "pt"  : np.arange(0, 10000, 500),
+  "eta" : np.arange(-3, 3, 0.1),
+  "deta" : np.arange(0, 6, 0.1),
+  "dphi" : np.arange(0, 5, 0.1),
+  "theta" : np.arange(0, 5, 0.1),
+  "dr" : np.arange(0, 5, 0.1),
+  "qOverP" : np.arange(0, 0.001, 0.0001),
+  "qOverPUncertainty" : np.arange(0, 0.001, 0.0001),
+  "phiUncertainty" : np.arange(0, 5, 0.1),
+  "thetaUncertainty" : np.arange(0, 5, 0.1),
+  "IP3D_signed_d0" : np.arange(-10, 10, 1),
+  "IP2D_signed_d0" : np.arange(-10, 10, 1),
+  "IP3D_signed_z0" : np.arange(-30, 30, 1),
+  "d0" : np.arange(-10, 10, 1),
+  "z0SinTheta" : np.arange(-30, 30, 1),
+  "d0Uncertainty" : np.arange(-1, 1, 0.1),
+  "z0SinThetaUncertainty" :  np.arange(-3, 3, 0.1),
+  "IP3D_signed_d0_significance" : np.arange(-30, 30, 1),
+  "IP3D_signed_z0_significance" : np.arange(-50, 50, 1),
+  "z0RelativeToBeamspot": np.arange(-50, 50, 1),
+  "z0RelativeToBeamspotUncertainty" : np.arange(-5, 5, 0.1),
+}
+
+variables = {
+    "hits": [
+        "numberOfInnermostPixelLayerHits",
+        "numberOfNextToInnermostPixelLayerHits",
+        "numberOfInnermostPixelLayerSharedHits",
+        "numberOfInnermostPixelLayerSplitHits",
+        "numberOfPixelHits",
+        "numberOfPixelHoles",
+        "numberOfPixelSharedHits",
+        "numberOfPixelSplitHits",
+        "numberOfSCTHits",
+        "numberOfSCTHoles",
+        "numberOfSCTSharedHits",
+        "expectNextToInnermostPixelLayerHit",
+        "expectInnermostPixelLayerHit",
+        "radiusOfFirstHit",
+    ],
+    "quality": [
+        "chiSquared",
+        "numberDoF",
+        "ptfrac",
+    ],
+    "kine": [
+        "pt",
+        "eta",
+        "deta",
+        "dphi",
+        "theta",
+        "dr",
+        "qOverP",
+        "qOverPUncertainty",
+        "phiUncertainty",
+        "thetaUncertainty",
+    ], 
+    "ips": [
+        "IP3D_signed_d0",
+        "IP2D_signed_d0",
+        "IP3D_signed_z0",
+        "d0",
+        "z0SinTheta",
+        "d0Uncertainty",
+        "z0SinThetaUncertainty",
+        "IP3D_signed_d0_significance",
+        "IP3D_signed_z0_significance",
+        "z0RelativeToBeamspot",
+        "z0RelativeToBeamspotUncertainty"
+    ], 
+}
+
+l_tracks_lrt = 0
+c_tracks_lrt = 0
+b_tracks_lrt = 0
+
+
+with File(input_file_lrt, 'r') as h5file:
+  tracks = h5file['tracks_from_jet']
+  #tracks = h5file['tracks']
+  jets = h5file['jets']
+
+  ljets = (jets["HadronConeExclTruthLabelID"] == 0).nonzero()[0]
+  cjets = (jets["HadronConeExclTruthLabelID"] == 4).nonzero()[0]
+  bjets = (jets["HadronConeExclTruthLabelID"] == 5).nonzero()[0]
+
+  l_tracks_lrt = tracks[ljets,:]
+  c_tracks_lrt = tracks[cjets,:]
+  b_tracks_lrt = tracks[bjets,:]
+
+  rownum_l,colnum_l=l_tracks_lrt.shape
+  rownum_c,colnum_c=c_tracks_lrt.shape
+  rownum_b,colnum_b=b_tracks_lrt.shape
+  
+  print("The dimensions of the l array for lrt container is, after cuts: " +str(rownum_l)+" by "+str(colnum_l))
+  print("The dimensions of the c array for lrt container is, after cuts: " +str(rownum_c)+" by "+str(colnum_c))
+  print("The dimensions of the b array for lrt container is, after cuts: " +str(rownum_b)+" by "+str(colnum_b))
+
+os.makedirs('combLRT_new',exist_ok=True)
+os.chdir('combLRT_new')
+
+for vartype in variables:
+  print("The vartype is: "+ vartype+ "\n")
+  for var in variables[vartype]:
+    print("The var is: "+ var +"\n")
+    lrt_l = l_tracks_lrt[l_tracks_lrt["valid"] == 1][var]
+    lrt_c = c_tracks_lrt[c_tracks_lrt["valid"] == 1][var]
+    lrt_b = b_tracks_lrt[b_tracks_lrt["valid"] == 1][var]
+    
+    plt.clf()
+    
+    plt.hist(lrt_l, color = ['r'], bins = Bins[var], histtype='step', alpha=0.5, density=True, label="ttbar LRT+STD corr to l-jets")
+    plt.hist(lrt_c, color = ['b'], bins = Bins[var], histtype='step', alpha=0.5, density=True, label="ttbar LRT+STD corr to c-jets")
+    plt.hist(lrt_b, color = ['g'], bins = Bins[var], histtype='step', alpha=0.5, density=True, label="ttbar LRT+STD corr to b-jets")
+    
+    plt.legend(loc='upper right',fontsize=10,framealpha=0.2)
+    plt.xlabel(var, size=14)
+    plt.ylabel("Normalized distribution (Method=density)", size=14)
+    plt.savefig(var + '_combined_lrt.png')
+    print("Savefig block done for "+var+".\n")
